f5c3be7e06085a5da900c632690fc6c5c8e0c802
Alonso Tacanga <alonso.tacanga@gmail.com>
2018-02-10 14:22:49 -0800

    updating deleting

--- server.js
+++ server.js
@@ -32,25 +32,69 @@ app.get('/api/players', (req, res) => {
 app.get('/api/players/:id', (req, res) => {
     db.Player.findOne({
         _id: req.params.id
-    }), (err, player) => {
+    }, (err, player) => {
         if (err) {
             console.log(`show error: ${err}`)
         } else {
             res.json(player)
         }
-    }
+    })
 });
 
 // create new 
 app.post('/api/players', (req, res) => {
+    let newPlayer = new db.Player(req.body);
+    newPlayer.save((err, player) => {
+        if (err) {
+            console.log(`save error: ${err}`)
+        } else {
+            console.log(`saved new player!: ${player.name}`);
+            res.json(player)
+        }
+    })
 });
 
 // delete one
 app.delete('/api/players/:id', (req, res) => {
+    let playerId = req.params.id;
+    db.Player.findOneAndRemove({
+        _id: playerId
+    })
+    .populate('player')
+    .exec((err, deletedPlayer) => {
+        res.json(deletedPlayer)
+    })
 });
 
 // update one
 app.put('/api/players/:id', (req, res) => {
+    let playerId = req.params.id;
+    db.Player.findOne({
+        _id: playerId
+    }, (err, foundPlayer) => {
+        if (err) {
+            console.log(`could not find the player`)
+        } else {
+            foundPlayer.name = req.body.name || foundPlayer.name;
+            foundPlayer.sport = req.body.sport || foundPlayer.sport;
+            foundPlayer.team = req.body.team || foundPlayer.team;
+            foundPlayer.jersey = req.body.jersey || foundPlayer.jersey;
+            foundPlayer.star = req.body.star || foundPlayer.star;
+            foundPlayer.imageUrl = req.body.imageUrl || foundPlayer.imageUrl;
+            console.log(`updating ${foundPlayer.name}`);
+            // save it
+            foundPlayer.save((err, player) => {
+                if (err) {
+                    console.log(`update error ${err}`)
+                } else {
+                    console.log(`updated player ${player.name}`);
+                    res.json(player)
+                }
+            })
+
+        }
+    })
+
 });
 
 // This is where we serve our API!
